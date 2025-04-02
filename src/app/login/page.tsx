2bcb5e134efa251fc450ace0797d8b9351e48c6d
Gabriel Glatz <gabriel.glatz@malwee.com.br>
2025-04-02 14:17:29 -0300

    feat:vercel

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -24,48 +24,48 @@ export default function Login() {
   };
   return (
     <div className="bg-[#2B2B2B]">
-      {" "}
-      <NavbarC />{" "}
+      
+      <NavbarC />
       <div className="min-h-[89.2vh] flex justify-center items-center py-32">
-        {" "}
+        
         <div className="bg-[#333333] p-8 rounded-lg w-auto min-w-lg">
-          {" "}
-          <h2 className="text-white text-2xl mb-4 font-semibold">Login</h2>{" "}
+          
+          <h2 className="text-white text-2xl mb-4 font-semibold">Login</h2>
           <form onSubmit={handleLogin} className="space-y-5">
-            {" "}
+            
             <div>
-              {" "}
-              <label className="text-white">Email:</label>{" "}
+              
+              <label className="text-white">Email:</label>
               <input
                 type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 className="bg-[#444444] text-white px-4 py-2 rounded-lg w-full mt-2"
-              />{" "}
-            </div>{" "}
+              />
+            </div>
             <div>
-              {" "}
-              <label className="text-white">Password:</label>{" "}
+              
+              <label className="text-white">Password:</label>
               <input
                 type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 className="bg-[#444444] text-white px-4 py-2 rounded-lg w-full mt-2"
-              />{" "}
-            </div>{" "}
+              />
+            </div>
             <div>
-              {" "}
+              
               <button
                 type="submit"
                 className="bg-[#BFAFF2] text-gray py-2 px-8 rounded-lg hover:bg-[#c9bcf4] w-full mt-4"
               >
-                {" "}
-                Login{" "}
-              </button>{" "}
-            </div>{" "}
-          </form>{" "}
-        </div>{" "}
-      </div>{" "}
+                
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
