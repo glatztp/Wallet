59da77a40531a098c39fdd9e141f72531e077f98
Gabriel Glatz <gabriel.glatz@malwee.com.br>
2025-04-02 14:10:15 -0300

    feat: vercel

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 import Image from "next/image"; 
+
 export default function Home() {
   return (
     <div className="bg-[#2B2B2B]">
@@ -32,8 +33,8 @@ export default function Home() {
             src="/Screenshot.svg"
             className="w-full h-auto bg-[#333333] rounded-2xl"
             alt="Screenshot"
-            width={500} 
-            height={500} 
+            width={500}
+            height={500}
           />
         </div>
       </div>
@@ -118,10 +119,7 @@ export default function Home() {
       <div className="flex justify-center items-center min-h-screen">
         <div className="bg-[#BFAFFA] w-[900px] h-[550px] flex flex-col justify-center items-center rounded-3xl">
           <h1 className="text-3xl font-semibold text-[#2b2b2b] text-justify opacity-80 py-10 px-20 -mt-10">
-            “Wallet is a great product! All of my most important information is
-            there - credit cards, transit cards, boarding passes, tickets, and
-            more. And I don't need to worry because it's all in one place!
-            thanks!”
+            “Wallet is a great product! All of my most important information is there - credit cards, transit cards, boarding passes, tickets, and more. And I don&apos;t need to worry because it&apos;s all in one place! thanks!”
           </h1>
 
           <div className="flex items-center justify-start mt-6 p-4 rounded-lg w-full max-w-[900px]">
@@ -164,12 +162,12 @@ export default function Home() {
       <div className="flex items-center justify-between h-screen px-40">
         <div className="flex flex-col justify-center w-1/2">
           <h1 className="text-6xl font-semibold text-white mb-8">Questions?</h1>
-          <h1 className="text-6xl font-semibold text-white">Let's Talk</h1>
+          <h1 className="text-6xl font-semibold text-white">Let&apos;s Talk</h1>
           <div className="text-lg tracking-wider">
             <p className="text-[#AFAFAF] mt-16">
               Contact us through our 24/7 live chat.{" "}
             </p>
-            <p className="text-[#AFAFAF] mb-16">We’re always happy to help!</p>
+            <p className="text-[#AFAFAF] mb-16">We&apos;re always happy to help!</p>
           </div>
           <div className="ml-0">
             <button className="bg-[#FFDF90] py-3 px-16 rounded-2xl hover:bg-[#FFDF80] transition-colors duration-100 mb-16">
