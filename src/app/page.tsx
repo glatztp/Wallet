018caba0e535d8e7028d65ae8b8d97140ff5e97b
Gabriel Glatz <gabriel.glatz@malwee.com.br>
2025-04-02 14:15:33 -0300

    feat: login and page adujsts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Navbar } from "../components/navbar";
+import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
+import Image from "next/image"; 
 
 export default function Home() {
   return (
@@ -28,28 +29,34 @@ export default function Home() {
           </div>
         </div>
         <div className="w-1/2 pl-16 pb-20">
-          <img
+          <Image
             src="/Screenshot.svg"
             className="w-full h-auto bg-[#333333] rounded-2xl"
             alt="Screenshot"
+            width={500}
+            height={500}
           />
         </div>
       </div>
 
       <div className="w-full">
-        <img
+        <Image
           src="/Logos.svg"
           className="w-full h-auto bg-[#333333] rounded-t-2xl"
           alt="Logos"
+          width={500}
+          height={500}
         />
       </div>
 
       <div className="flex justify-center items-center min-h-screen">
         <div className="bg-white w-[900px] h-[510px] flex justify-center items-center rounded-3xl">
-          <img
+          <Image
             src="/HomeScnd.svg"
             className="h-full w-auto min-w-full min-h-full object-contain mt-24"
             alt="Centralized Image"
+            width={500}
+            height={500}
           />
         </div>
       </div>
@@ -58,10 +65,12 @@ export default function Home() {
         <div className="w-full max-w-6xl px-8">
           <ul className="flex space-x-12 justify-between items-center text-center">
             <li className="flex flex-col items-center">
-              <img
+              <Image
                 src="/Cards.svg"
                 className="w-12 h-12 mb-4"
                 alt="Cards Icon"
+                width={50}
+                height={50}
               />
               <h3 className="text-xl font-semibold text-white mt-6">
                 Customizable Card
@@ -72,7 +81,13 @@ export default function Home() {
             </li>
 
             <li className="flex flex-col items-center">
-              <img src="/Coin.svg" className="w-12 h-12 mb-4" alt="Coin Icon" />
+              <Image
+                src="/Coin.svg"
+                className="w-12 h-12 mb-4"
+                alt="Coin Icon"
+                width={50}
+                height={50}
+              />
               <h3 className="text-xl font-semibold text-white mt-6">
                 No Payment Fee
               </h3>
@@ -82,10 +97,12 @@ export default function Home() {
             </li>
 
             <li className="flex flex-col items-center">
-              <img
+              <Image
                 src="/Purse.svg"
                 className="w-12 h-12 mb-4"
                 alt="Purse Icon"
+                width={50}
+                height={50}
               />
               <h3 className="text-xl font-semibold text-white mt-6">
                 All in One Place
@@ -102,19 +119,18 @@ export default function Home() {
       <div className="flex justify-center items-center min-h-screen">
         <div className="bg-[#BFAFFA] w-[900px] h-[550px] flex flex-col justify-center items-center rounded-3xl">
           <h1 className="text-3xl font-semibold text-[#2b2b2b] text-justify opacity-80 py-10 px-20 -mt-10">
-            “Wallet is a great product! All of my most important information is
-            there - credit cards, transit cards, boarding passes, tickets, and
-            more. And I don't need to worry because it's all in one place!
-            thanks!”
+            “Wallet is a great product! All of my most important information is there - credit cards, transit cards, boarding passes, tickets, and more. And I don&apos;t need to worry because it&apos;s all in one place! thanks!”
           </h1>
 
           <div className="flex items-center justify-start mt-6 p-4 rounded-lg w-full max-w-[900px]">
             <div className="flex items-center justify-start pl-16">
               <div className="w-20 h-20">
-                <img
+                <Image
                   src="/Profile.svg"
                   className="w-full h-auto rounded-full"
                   alt="Profile Icon"
+                  width={50}
+                  height={50}
                 />
               </div>
 
@@ -136,77 +152,22 @@ export default function Home() {
                       d="M12 17.27l6.18 3.73-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73-1.64 7.03L12 17.27z"
                     />
                   </svg>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="full"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    className="w-5 h-5 text-[#2B2B2B] opacity-70"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M12 17.27l6.18 3.73-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73-1.64 7.03L12 17.27z"
-                    />
-                  </svg>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="full"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    className="w-5 h-5 text-[#2B2B2B] opacity-70"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M12 17.27l6.18 3.73-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73-1.64 7.03L12 17.27z"
-                    />
-                  </svg>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="full"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    className="w-5 h-5 text-[#2B2B2B] opacity-70"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M12 17.27l6.18 3.73-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73-1.64 7.03L12 17.27z"
-                    />
-                  </svg>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="full"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    className="w-5 h-5 text-[#2b2b2b] opacity-70"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M12 17.27l6.18 3.73-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73-1.64 7.03L12 17.27z"
-                    />
-                  </svg>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+
       <div className="flex items-center justify-between h-screen px-40">
         <div className="flex flex-col justify-center w-1/2">
           <h1 className="text-6xl font-semibold text-white mb-8">Questions?</h1>
-          <h1 className="text-6xl font-semibold text-white">Let's Talk</h1>
+          <h1 className="text-6xl font-semibold text-white">Let&apos;s Talk</h1>
           <div className="text-lg tracking-wider">
             <p className="text-[#AFAFAF] mt-16">
               Contact us through our 24/7 live chat.{" "}
             </p>
-            <p className="text-[#AFAFAF] mb-16">We’re always happy to help!</p>
+            <p className="text-[#AFAFAF] mb-16">We&apos;re always happy to help!</p>
           </div>
           <div className="ml-0">
             <button className="bg-[#FFDF90] py-3 px-16 rounded-2xl hover:bg-[#FFDF80] transition-colors duration-100 mb-16">
@@ -215,13 +176,16 @@ export default function Home() {
           </div>
         </div>
         <div className="w-1/2 pl-16 pb-20">
-          <img
+          <Image
             src="/SecndScreen.svg"
             className="w-full h-auto bg-[#333333] rounded-2xl"
             alt="Screenshot"
+            width={500}
+            height={500}
           />
         </div>
       </div>
+
       <Footer />
     </div>
   );
